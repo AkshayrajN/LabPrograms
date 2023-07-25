59bce6ab64eb9be62b151bf944ab95d0a8fe0009
AkshayRaj N <94297346+AkshayrajN@users.noreply.github.com>
2023-07-25 12:04:05 +0530

    Create Exception_7.java

---
+++ OOPJ/Exception_7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ArthExcep{
+public static void main(String []args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter two numbers");
+int a=sc.nextInt();
+int b=sc.nextInt();
+double ans=0;
+try{
+        ans=a/b;
+        System.out.println(ans);
+
+}
+catch(ArithmeticException e){
+        System.out.println("Exception Caught");
+  }
+
+}
+}
